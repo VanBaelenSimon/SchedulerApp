0816d47bf894f989d0e8deb9e1bdf3fc1a4243a6
Simon Van Baelen <99735851+VanBaelenSimon@users.noreply.github.com>
2025-09-18 10:50:54 +0200

    Update Profile.vue
    Update profile page

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="pages">
-    <h1>Profile page</h1>
-    <p>User profile info goes here.</p>
     <button @click="auth.logout">Logout</button>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { useAuthStore } from '../store/auth';
-  const auth = useAuthStore();
+import { useAuthStore } from '../store/auth';
+const auth = useAuthStore();
 </script>
 
 <style scoped></style>
